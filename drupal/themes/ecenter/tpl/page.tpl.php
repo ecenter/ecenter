f73a5909921996b09f291f9970beae72676c0bb7
David Eads <davideads@gmail.com>
2010-05-14 13:25:26 -0500

    Removed wrong markup from the page template a few commits ago;  add the site name and remove the silly logo-title markup.

--- drupal/themes/ecenter/tpl/page.tpl.php
+++ drupal/themes/ecenter/tpl/page.tpl.php
@@ -21,13 +21,9 @@
 <body class="<?php print $body_classes; ?>">
 
 <div id="header-wrapper">
-  <div id="logo-title">
-    <?php if (!empty($logo)): ?>
-    <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" id="logo">
-      <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" />
-    </a>
-    <?php endif; ?>
-  </div>
+  <h1 id="site-name">
+    <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home"><<?php print $site_name; ?></a>
+  </h1>
 
   <?php if (!empty($header)): ?>
   <div id="header">

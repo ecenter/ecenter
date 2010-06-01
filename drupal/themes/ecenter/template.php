beda99f15b90489cb0b1b159353a1b85c32f178a
David Eads <davideads@gmail.com>
2010-06-01 14:06:47 -0500

    Added beginnings of wiki page editing overrides.

--- drupal/themes/ecenter/template.php
+++ drupal/themes/ecenter/template.php
@@ -5,10 +5,29 @@
  * @file
  * The E-center theme.
  */
+function ecenter_theme() {
+  $path = drupal_get_path('theme', 'ecenter') .'/tpl';
+  return array(
+    'wiki_node_form' => array('arguments' => array('form' => NULL), 'template' => 'wiki-node-form', 'path' => $path),
+  );
+}
 
 /**
  * Preprocess page
  */
 function ecenter_preprocess_page(&$vars) {
   // Hello, world.
+}
+
+function ecenter_preprocess_wiki_node_form(&$vars) {
+  $form = $vars['form'];
+
+  // Render some elements individually
+  $vars['title'] = drupal_render($form['title']);
+  $vars['buttons'] = drupal_render($form['buttons']);
+  $vars['image_attachments'] = drupal_render($form['field_image']);
+  $vars['body'] = drupal_render($form['body_field']);
+
+  // Catch all
+  $vars['form'] = drupal_render($form);
 }

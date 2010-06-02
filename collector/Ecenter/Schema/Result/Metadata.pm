65d8a0be6b9b0e3498949160e5f2898d53c654b8
Maxim Grigoriev <maxim@fnal.gov>
2010-06-02 22:02:41 +0000

    r103: changed schema to support ipv6

--- collector/Ecenter/Schema/Result/Metadata.pm
+++ collector/Ecenter/Schema/Result/Metadata.pm
@@ -2,13 +2,11 @@ package Ecenter::Schema::Result::Metadata;
 use base qw/DBIx::Class::Core/;
 
 __PACKAGE__->table('metadata');
-__PACKAGE__->add_columns(qw/metadata metaid src_ip dst_ip rtr_ip capacity service subject parameters/); 
-__PACKAGE__->set_primary_key('metadata');
+__PACKAGE__->add_columns(qw/metaid perfsonar_id src_ip dst_ip rtr_ip capacity service subject parameters/); 
+__PACKAGE__->set_primary_key('metaid');
 __PACKAGE__->belongs_to(service => 'Ecenter::Schema::Result::Service');
 __PACKAGE__->belongs_to(src_ip => 'Ecenter::Schema::Result::Node',  { 'foreign.ip_addr' => 'self.src_ip' }  );
-__PACKAGE__->belongs_to(dst_ip => 'Ecenter::Schema::Result::Node',  { 'foreign.ip_addr' => 'self.dst_ip' } );
-__PACKAGE__->belongs_to(rtr_ip  => 'Ecenter::Schema::Result::Node', { 'foreign.ip_addr' => 'self.rtr_ip' }  );
-
+ 
 __PACKAGE__->has_many(datas => 'Ecenter::Schema::Result::Data');
 __PACKAGE__->has_many(pinger_datas => 'Ecenter::Schema::Result::Pinger_data');
 __PACKAGE__->has_many(bwctl_datas => 'Ecenter::Schema::Result::Bwctl_data');
@@ -18,7 +16,7 @@ __PACKAGE__->has_many(traceroute_datas => 'Ecenter::Schema::Result::Traceroute_d
 
 
 
-__PACKAGE__->add_unique_constraint( metaid_service => [ qw/metaid service/ ]);
+__PACKAGE__->add_unique_constraint( metaid_service => [ qw/perfsonar_id service/ ]);
 
 
 1;
--- collector/Ecenter/Schema/Result/Node.pm
+++ collector/Ecenter/Schema/Result/Node.pm
@@ -2,8 +2,10 @@ package Ecenter::Schema::Result::Node;
 use base qw/DBIx::Class::Core/;
 
 __PACKAGE__->table('node');
-__PACKAGE__->add_columns(qw/ip_addr nodename ipv4_dot/);
+__PACKAGE__->add_columns(qw/ip_addr nodename ip_noted/);
 __PACKAGE__->set_primary_key('ip_addr');
-__PACKAGE__->has_many(metadatas =>  'Ecenter::Schema::Result::Metadata', 'metadata');
-__PACKAGE__->has_many(hops =>  'Ecenter::Schema::Result::Hop',    { 'foreign.hop_ip' => 'self.ip_addr' } );
+__PACKAGE__->has_many(metadatas =>  'Ecenter::Schema::Result::Metadata',  { 'foreign.src_ip' => 'self.ip_addr' });
+  
+
+__PACKAGE__->has_many(hops =>  'Ecenter::Schema::Result::Hop',   'hop_ip');
 1;

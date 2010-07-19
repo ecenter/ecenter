87841d34b31575ce0d60cd62251e59e5181fbb25
David Eads <davideads@gmail.com>
2010-07-19 09:11:23 -0500

    Added more aggregate value juggling, added inline CSS.

--- drupal/modules/ecenter/ecenter_weathermap/js/display.js
+++ drupal/modules/ecenter/ecenter_weathermap/js/display.js
@@ -1,9 +1,9 @@
 // @TODO Currently REALLY ROUGH!!!
-Drupal.behaviors.EcenterTraceroute = function(context) {
+/*Drupal.behaviors.EcenterTraceroute = function(context) {
    $('.hop-wrapper div').hide(); 
    $('.hop-wrapper').hover(function() {
      $('div', $(this)).show();
    }, function() {
      $('div', $(this)).hide();
    });
-}
+}*/

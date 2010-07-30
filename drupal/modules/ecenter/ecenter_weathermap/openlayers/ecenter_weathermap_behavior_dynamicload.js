7b38a35b5b5d27a4fa2997b57af4794a7db1cec0
David Eads <davideads@gmail.com>
2010-07-30 11:28:42 -0500

    Really frustrated with getting this dynamic mapping to work.

--- drupal/modules/ecenter/ecenter_weathermap/openlayers/ecenter_weathermap_behavior_dynamicload.js
+++ drupal/modules/ecenter/ecenter_weathermap/openlayers/ecenter_weathermap_behavior_dynamicload.js
@@ -1,22 +1,29 @@
-Drupal.behaviors.ecenter_weathermap_behavior_dynamicload = function(context) {
+/*Drupal.behaviors.ecenter_weathermap_behavior_dynamicload = function(context) {
   for (var key in Drupal.settings.openlayers.maps) {
     var map = Drupal.settings.openlayers.maps[key];
     var data = $('#' + key).data('openlayers');
     if (data) {
-      /*for (var name in data.openlayers.layers) {
+      for (var name in data.openlayers.layers) {
         var layer = data.openlayers.layers[name];
         if (!layer.isBaseLayer) {
           layer.destroy();
         }
       }
-      Drupal.openlayers.addLayers(map, data.openlayers);*/
-      /*for (var name in data.openlayers.layers) {
-        var layer = data.openlayers.layers[name];
-        //if (!layer.isBaseLayer) {
-          layer.redraw();
-        //}
-      }*/
-      console.log(data.openlayers);
+      //console.log(map);
+      for (var name in map.layers) {
+        if (map.layers[name].baselayer) {
+          console.log(name);
+          delete map.layers[name];
+        }
+      }
+      Drupal.openlayers.addLayers(map, data.openlayers);
+      //for (var name in data.openlayers.layers) {
+      //  var layer = data.openlayers.layers[name];
+      //  if (!layer.isBaseLayer) {
+      //    layer.redraw();
+      //  }
+      //}
+      //console.log(data.openlayers);
     }
   }
-}
+}*/

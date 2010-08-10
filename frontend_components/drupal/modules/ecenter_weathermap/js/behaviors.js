191d9ba2aec13ccd682004c9ed4fff8dd3e0d148
David Eads <daveeads@fnal.gov>
2010-08-10 18:16:46 +0000

    r178: Added changes since last meeting: Improved graph rendering, split inline css module out, added views export code module, refactored theming, and fixed a number of bugs.

--- frontend_components/drupal/modules/ecenter_weathermap/js/behaviors.js
+++ frontend_components/drupal/modules/ecenter_weathermap/js/behaviors.js
@@ -22,3 +22,28 @@ Drupal.behaviors.EcenterEvents = function(context) {
     $('#ip-select-wrapper input#edit-ip-select-dst-ip-wrapper-dst-ip_quickselect').val('').attr('disabled', true);
   });
 }
+
+/**
+ * Disable form elements during AHAH request
+ *
+ * Adding this to Drupal behaviors causes some serious chaos because it winds
+ * up getting called many times, which the behavior only needs to be bound
+ * and executed once.
+ */
+$(document).ready(function() {
+  $('#ecenter-weathermap-select-form').bind('ahah_start', function() {
+    $(this).addClass('data-loading');
+    $('input', this).each(function() {
+      var disabled = $(this).attr('disabled');
+      $(this).data('ecenter_disabled', disabled);
+      $(this).attr('disabled', true);
+    });
+  });
+  $('#ecenter-weathermap-select-form').bind('ahah_end', function() {
+    $(this).removeClass('data-loading');
+    $('input', this).each(function() {
+      var disabled = $(this).data('ecenter_disabled');
+      $(this).attr('disabled', disabled);
+    });
+  });
+});

bb9ad3d30441e85c4f80724f8fea478bc3ac80c5
David Eads <davideads@gmail.com>
2010-10-12 13:18:32 -0500

    Removed breadcrumb from page template, removed too-specific form-item CSS rule.

--- drupal/themes/ecenter/tpl/page.tpl.php
+++ drupal/themes/ecenter/tpl/page.tpl.php
@@ -50,12 +50,6 @@
 </div>
 <?php endif; ?>
 
-<?php if ($breadcrumb): ?>
-<div id="breadcrumb">
-  <?php print $breadcrumb; ?>
-</div>
-<?php endif; ?>
-
 <?php if (!empty($tabs)): ?>
   <div id="tasks" class="clearfix">
     <?php print $tabs; ?>

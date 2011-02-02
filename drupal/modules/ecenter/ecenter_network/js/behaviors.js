39f83f9f6d6a2d091e70cc26dfa372092c946eec
David Eads <davideads@gmail.com>
2011-02-02 17:57:51 -0600

    Make show button work more generically.

--- drupal/modules/ecenter/ecenter_network/js/behaviors.js
+++ drupal/modules/ecenter/ecenter_network/js/behaviors.js
@@ -45,9 +45,9 @@ Drupal.behaviors.EcenterShowTables = function(context) {
   var hide_label = Drupal.t('Hide data tables');
   $('.tablechart', context).after('<button class="toggle-data">' + show_label + '</button>');
   $('.toggle-data', context).toggle(function() {
-    $(this).text(hide_label).parent().find('#utilization-tables').slideDown('fast');
+    $(this).text(hide_label).parent().find('table').slideDown('fast');
   }, function() {
-    $(this).text(show_label).parent().find('#utilization-tables').slideUp('fast');
+    $(this).text(show_label).parent().find('table').slideUp('fast');
   });
 }
 

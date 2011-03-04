d90736075e4e369a9d7fb7548dc2598941d0865f
David Eads <davideads@gmail.com>
2011-03-04 13:19:57 -0600

    Major overhaul to response parser: neatly categorize data as 'hop', 'traceroute', and 'end_to_end'.

--- drupal/modules/ecenter/ecenter_network/theme/ecenter-network-data.tpl.php
+++ drupal/modules/ecenter/ecenter_network/theme/ecenter-network-data.tpl.php
@@ -1,23 +1,13 @@
-<div id="results-toolbar" class="clear-block">
+<div id="results-title" class="clearfix">
   <h2><?php print $title; ?></h2>
-  <?php if ($issuelink || $permalink): ?>
-  <div class="links">
-    <?php if ($issuelink): ?>
-    <?php print $issuelink; ?>
-    <?php endif; ?>
-
-    <?php if ($permalink): ?>
-    <?php print $permalink; ?>
-    <?php endif; ?>
-  </div>
+  <?php if ($permalink): ?>
+  <?php print $permalink; ?>
   <?php endif; ?>
 </div>
 
 <div id="results">
   <div id="hop-wrapper" class="clear-block">
-    <div id="snmp-results">
-      <?php print $snmp; ?>
-    </div>
+    <?php print $hops; ?>
   </div>
   <div id="end-to-end-wrapper" class="clear-block">
     <?php print $end_to_end; ?>

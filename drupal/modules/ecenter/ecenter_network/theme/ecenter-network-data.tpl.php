d0d406096e12a34c72fd503690fe5aec852d6c3c
David Eads <davideads@gmail.com>
2011-03-04 14:07:34 -0600

    Rejiggered title and permalink in data display.

--- drupal/modules/ecenter/ecenter_network/theme/ecenter-network-data.tpl.php
+++ drupal/modules/ecenter/ecenter_network/theme/ecenter-network-data.tpl.php
@@ -1,15 +1,17 @@
-<div id="results-title" class="clearfix">
-  <h2><?php print $title; ?></h2>
-  <?php if ($permalink): ?>
-  <?php print $permalink; ?>
-  <?php endif; ?>
-</div>
-
 <div id="results">
-  <div id="hop-wrapper" class="clear-block">
+  <div id="results-title" class="clearfix">
+    <h2><?php print $title; ?></h2>
+    <?php if ($permalink): ?>
+    <div class="permalink">
+      <?php print $permalink_field; ?>
+      <?php print $permalink; ?>
+    </div>
+    <?php endif; ?>
+  </div>
+  <div id="hop-wrapper" class="clearfix">
     <?php print $hops; ?>
   </div>
-  <div id="end-to-end-wrapper" class="clear-block">
+  <div id="end-to-end-wrapper" class="clearfix">
     <?php print $end_to_end; ?>
   </div>
 </div>

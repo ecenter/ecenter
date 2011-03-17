0a1fdc2e4f88b6ec7518aa12b4c7193d813d519a
Netadmin <netadmin@xenmon.fnal.gov>
2011-03-17 16:39:26 -0500

    fixed bugs, added hubs subnets  from the ARIN

--- collector/Ecenter/Data/Hub.pm
+++ collector/Ecenter/Data/Hub.pm
@@ -6,11 +6,14 @@ use FindBin qw($RealBin);
 use lib  "$FindBin::Bin";
  
 use English qw( -no_match_vars ); 
-
+use Data::Dumper;
 use Log::Log4perl qw(get_logger);
 use Net::Netmask; 
 use Ecenter::Types qw(HubName IP_addr);
 use MooseX::Params::Validate;
+use JSON::XS  qw(decode_json);
+use LWP::UserAgent;
+
 
 =head1 NAME
 
@@ -34,30 +37,87 @@ use MooseX::Params::Validate;
 
 =back
 
-=head2 
+=head1 METHODS
 
 =cut
+
 has 'logger'     => (is => 'rw', isa => 'Log::Log4perl::Logger');
 has 'hub_name' => (is => 'rw', isa => 'Ecenter::Types::HubName');
-my %hubs = ( FNAL => {'131.225.0.0' => 16, '198.49.208.0' => 24},
-             LBL => {'131.243.0.0' => 16, '128.3.121.0' => 24},
-	     ORNL => {'192.31.0.0' => 16 },
-	     SLAC => {'134.79.0.0' => 16, '198.129.191.0' => 24},
-	     BNL => {'192.12.0.0' => 16},
-	     ANL => {'164.54.56.0' => 24, '146.137.252.0' => 24, '130.202.222.0' => 24, '140.221.83.0' => 24},
-	   );  
- 
+# handle is the registered handle at whois.arin.net  - ARIN IP registar
+my %HUBS = ( FNAL  => {nets => {'131.225.0.0' => 16, '198.49.208.0' => 24}, handle => 'FERMIL'}, #FERMIL
+             LBL   => {nets => {'131.243.0.0' => 16, '128.3.121.0' => 24},  handle => 'LBNL'}, # LBNL
+	     ORNL  => {nets => {'192.31.0.0' => 16 },                       handle => 'ORNL'},
+	     SLAC  => {nets => {'134.79.0.0' => 16,  '198.51.111.0' => 24}, handle => 'THELE-44-Z'},
+	     BNL   => {nets => {'192.12.15.0' => 24},                       handle => 'BNL'},
+	     ANL   => {nets => {'164.54.0.0' => 16,  '146.137.0.0' => 16, '130.202.0.0' => 16, '140.221.0.0' => 16}, handle => 'ANLB'},
+	     NERSC => {nets => {'128.55.00.0'  => 16},                      handle => 'NET-128-55-0-0-1'},
+	     PNL   => {nets => {'192.101.100.0'=> 22,'192.101.104.0' => 22},handle => 'PNNL-Z'},
+	     NASA  => {nets => {'198.9.0.0' => 16},                         handle => 'NASA'},
+	     PPPL  => {nets => {'192.188.10.0' => 24},                      handle => 'PPPL'},
+	     LLNL  => {nets => {'198.128.240.0' => 20},                     handle => 'LLNL-1'},
+	   );
+my $WHOIS_ORG = 'http://whois.arin.net/rest/org'; # add /<handle>/nets.json	
+my $WHOIS_NET = 'http://whois.arin.net/rest/net';  # add /<handle>.json
+
 sub BUILD {
       my $self = shift;
    
       $self->logger(get_logger(__PACKAGE__));  
 };
 
+=head2 get_ips
+
+=cut
+
 sub get_ips {
     my $self = shift;
     $self->_set_hub(@_);
-    return  $hubs{$self->hub_name};
+    return  $HUBS{$self->hub_name}{nets};
 }; 
+
+=head2 get_hub_blocks
+
+get IP blocks from the ARIN registrant - should be run once on the whole object to populate %HUBS - takes some time.
+returns list of HUB names
+
+=cut
+
+sub get_hub_blocks {
+    my $self = shift;
+    my $mech =   LWP::UserAgent->new(agent => 'Mozilla'); ## some unique id will be added
+    $mech->default_header( 'Content-Type' => 'application/json' );
+    foreach my $handle (keys %HUBS) {
+        my $response = $mech->get( ($HUBS{$handle}{handle} =~ /^NET-/?"$WHOIS_NET/$HUBS{$handle}{handle}.json":"$WHOIS_ORG/$HUBS{$handle}{handle}/nets.json") );
+        if ($response->is_success) {
+            my $nets_obj = decode_json($response->content);
+	    my @nets = ();
+	    if(exists $nets_obj->{nets}) {
+	        foreach my $net_el ( @{$nets_obj->{nets}{netRef}} ) {
+	            if(exists  $net_el->{'$'}) {
+	                 my  $resp_net = $mech->get( $net_el->{'$'} . '.json' );
+                         if($resp_net->is_success) {
+                             my $net_obj = decode_json($resp_net->content);
+			     my $net =  $self->_get_net( $net_obj );
+			     push @nets, @{$net} if $net;
+			 }
+		    }
+	        }
+	    } elsif(exists $nets_obj->{net}) {
+	        my $net = $self->_get_net( $nets_obj );
+	        push @nets, @{$net} if $net;
+	    }  
+	    map { $HUBS{$handle}{nets}{$_->[0]} = $_->[1]} @nets;
+        }   else {
+            $self->logger->debug( " Skipping this handle: $handle due " .$response->status_line);
+            next;
+        }
+    }
+    return keys %HUBS;
+}; 
+
+=head2 match
+
+=cut
 
 sub match {
  my ( $self, %arg ) =   validated_hash(
@@ -69,6 +129,21 @@ sub match {
 	if($block->match($arg{ip}->ip())) {
 	   return 1;
 	}
+    }
+    return;
+};
+
+sub _get_net {
+    my ($self,  $net_obj ) =  @_;
+    if(exists $net_obj->{net} && $net_obj->{net}{netBlocks} && $net_obj->{net}{netBlocks}{netBlock}  ) {
+        $self->logger->debug("netBlock::", sub{Dumper($net_obj->{net}{netBlocks}{netBlock} )});
+	$net_obj->{net}{netBlocks}{netBlock} = [ $net_obj->{net}{netBlocks}{netBlock} ] 
+	   unless ref $net_obj->{net}{netBlocks}{netBlock} eq ref [];
+	my @return = ();
+	foreach my $block (@{$net_obj->{net}{netBlocks}{netBlock}}) {
+     	    push @return, [$block->{startAddress}{'$'}, $block->{cidrLength}{'$'}];
+	}
+	return \@return;
     }
     return;
 };

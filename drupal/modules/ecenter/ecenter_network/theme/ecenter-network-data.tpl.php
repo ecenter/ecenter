9bb67bf004d662d7f9d8dcbe08c951ddf1afde18
David Eads <davideads@gmail.com>
2011-03-18 14:56:32 -0500

    Tweaked issue and permalink button placement in results.

--- drupal/modules/ecenter/ecenter_network/theme/ecenter-network-data.tpl.php
+++ drupal/modules/ecenter/ecenter_network/theme/ecenter-network-data.tpl.php
@@ -4,22 +4,27 @@
     </div> */ ?>
   <div id="hop-wrapper" class="clearfix">
     <?php print $hops; ?>
+
+    <div id="result-links-wrapper"
+      <?php if ($issuelink): ?>
+      <div class="issuelink">
+        <?php print $issuelink; ?>
+      </div>
+      <?php endif; ?>
+
+      <?php if ($permalink): ?>
+      <div class="permalink">
+        <?php print $permalink; ?>
+        <?php print $permalink_field; ?>
+      </div>
+      <?php endif; ?>
+    </div>
+
   </div>
+    
   <div id="end-to-end-wrapper" class="clearfix">
     <?php print $end_to_end; ?>
   </div>
 
-  <?php if ($issuelink): ?>
-  <div class="issuelink">
-    <?php print $issuelink; ?>
-  </div>
-  <?php endif; ?>
-
-  <?php if ($permalink): ?>
-  <div class="permalink">
-    <?php print $permalink_field; ?>
-    <?php print $permalink; ?>
-  </div>
-  <?php endif; ?>
 
 </div>

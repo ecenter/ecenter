4235ac15cb1f3ffb2faf83194d9d913f9149b8b4
David Eads <davideads@gmail.com>
2011-03-28 17:28:20 -0500

    Added submitted line to node template.

--- drupal/themes/ecenter/tpl/node.tpl.php
+++ drupal/themes/ecenter/tpl/node.tpl.php
@@ -7,6 +7,8 @@
   <h2><a href="<?php print $node_url ?>" title="<?php print $title ?>"><?php print $title ?></a></h2>
 <?php endif; ?>
 
+  <?php print $submitted; ?>
+
   <div class="content">
     <?php print $content ?>
   </div>

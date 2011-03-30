f0012b6ab4c503c3bff679cee1080c97277c52ea
David Eads <davideads@gmail.com>
2011-03-30 15:32:31 -0500

    Updated query links to use proper query UI for the moment. At least it works.

--- drupal/themes/ecenter/template.php
+++ drupal/themes/ecenter/template.php
@@ -5,4 +5,7 @@ function ecenter_preprocess_page(&$vars) {
   if (arg(0) == 'network' || arg(0) == '<front>') {
     unset($vars['breadcrumb']);
   }
+  if (arg(0) == 'network' && $_GET['modal']) {
+    $vars['body_classes'] .= ' modal';
+  }
 }

2e71268a177ecc5085d725ff77b0b2edfbf07b8b
Maxim Grigoriev <maxim@fnal.gov>
2011-04-25 10:37:20 -0500

    ADS detector placeholder module and perl client API

--- collector/Ecenter/ADS/Detector.pm
+++ collector/Ecenter/ADS/Detector.pm
@@ -6,26 +6,36 @@ use namespace::autoclean;
 use FindBin qw($RealBin);
 use lib  "$FindBin::Bin";
 
+use Ecenter::DataClient;
+
 use Log::Log4perl qw(get_logger);
 use English qw( -no_match_vars );
 
 =head1 NAME
 
- E-Center::ADS::Detector -  base  class for the Anomalous Detection Algorithm implmentation
+E-Center::ADS::Detector -  base  class for the Anomalous Detection Algorithm implmentation
 
 =head1 DESCRIPTION
 
-base client for the   Anomalous Detection Algorithm implmentation. Assumed to be subclassed for some
-more specific algorithms.
-  
+base calss for the Anomalous Detection Algorithm implmentation. Might be subclassed for the
+more specific analysis implementations.
+It accepts some parameters ( named attributes to the class ) and sends request to the E-Center DRS by
+utilizing Ecenter::DataClient, then it processess the data from the DRS and returns results where results
+are stored in the 'results' attribute.
+
 =head1 SYNOPSIS 
- 
+
 see Ecenter::DataClient, Ecenter::Client for the subclassing examples. Normal usage:
 
  use Moose;
  extends 'Ecenter::ADS::Detector';
 
-
+ ## or, if no subclasses required then somehting like
+   
+  my $detector = Ecenter::ADS::Detector->new({ data => \@data_array });
+  my $results = $detector->process_data;
+  ### and results are in the results attribute:
+  $results = $detector->results;
 
 =head1 ATTRIBUTES
 
@@ -45,8 +55,9 @@ logging  agent via C<Log::Log4perl>
 
 =cut
 
-
-has logger     => (is => 'rw', isa => 'Log::Log4perl::Logger');
+has data    =>  (is => 'rw', isa => 'ArrayRef');
+has results => (is => 'rw', isa => 'ArrayRef');
+has logger  => (is => 'rw', isa => 'Log::Log4perl::Logger');
 
 sub BUILD {
     my ($self, $args) = @_;
@@ -54,7 +65,17 @@ sub BUILD {
     map {$self->$_($args->{$_}) if $self->can($_)}  keys %$args if $args && ref $args eq ref {};
 }
 
+=head1 process_data
 
+  data processing 
+
+=cut
+
+
+sub process_data {
+
+
+}
 
 no Moose;
 __PACKAGE__->meta->make_immutable;

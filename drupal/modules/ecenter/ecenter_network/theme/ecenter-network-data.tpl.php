e413b5a8246482e73c7a6c89e337c448cebc4135
David Eads <davideads@gmail.com>
2011-04-26 12:02:22 -0500

    Cleaned up handling, logging, display of missing data.

--- drupal/modules/ecenter/ecenter_network/theme/ecenter-network-data.tpl.php
+++ drupal/modules/ecenter/ecenter_network/theme/ecenter-network-data.tpl.php
@@ -1,37 +1,30 @@
-<div id="results">
-  <?php /* <div id="results-title" class="clearfix">
+<div id="hop-wrapper" class="clearfix">
+  <?php print $hops; ?>
+
+  <div id="result-links-wrapper">
+
     <h2><?php print $title; ?></h2>
-    </div> */ ?>
-  <div id="hop-wrapper" class="clearfix">
-    <?php print $hops; ?>
 
-    <div id="result-links-wrapper">
-
-      <h2><?php print $title; ?></h2>
-
-      <?php if ($issuelink): ?>
-      <div class="issuelink">
-        <?php print $issuelink; ?>
-      </div>
-      <?php endif; ?>
-      <?php if ($issues): ?>
-      <div class="issues">
-        <?php print $issues; ?>
-      </div>
-      <?php endif; ?>
-      <?php if ($permalink): ?>
-      <div class="permalink">
-        <?php print $permalink; ?>
-        <?php print $permalink_field; ?>
-      </div>
-      <?php endif; ?>
+    <?php if ($issuelink): ?>
+    <div class="issuelink">
+      <?php print $issuelink; ?>
     </div>
-
+    <?php endif; ?>
+    <?php if ($issues): ?>
+    <div class="issues">
+      <?php print $issues; ?>
+    </div>
+    <?php endif; ?>
+    <?php if ($permalink): ?>
+    <div class="permalink">
+      <?php print $permalink; ?>
+      <?php print $permalink_field; ?>
+    </div>
+    <?php endif; ?>
   </div>
-    
-  <div id="end-to-end-wrapper" class="clearfix">
-    <?php print $end_to_end; ?>
-  </div>
 
-
+</div>
+  
+<div id="end-to-end-wrapper" class="clearfix">
+  <?php print $end_to_end; ?>
 </div>

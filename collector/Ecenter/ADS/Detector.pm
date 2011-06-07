2de6e3c795def73666a6efb9b6820fe39fa421bf
Maxim Grigoriev <maxim@fnal.gov>
2011-06-07 14:54:53 -0500

    compiled ADS version - before testing -

--- collector/Ecenter/ADS/Detector.pm
+++ collector/Ecenter/ADS/Detector.pm
@@ -2,12 +2,7 @@ package Ecenter::ADS::Detector;
 
 use Moose;
 use namespace::autoclean;
-
-use FindBin qw($RealBin);
-use lib  "$FindBin::Bin";
-
-use Ecenter::DataClient;
-
+ 
 use Log::Log4perl qw(get_logger);
 use English qw( -no_match_vars );
 
@@ -19,8 +14,8 @@ E-Center::ADS::Detector -  base  class for the Anomalous Detection Algorithm imp
 
 base calss for the Anomalous Detection Algorithm implmentation. Might be subclassed for the
 more specific analysis implementations.
-It accepts some parameters ( named attributes to the class ) and sends request to the E-Center DRS by
-utilizing Ecenter::DataClient, then it processess the data from the DRS and returns results where results
+It accepts some parameters ( named attributes to the class ) and  parses data from the 'data' attribute and 
+then it processess  data and returns results where results
 are stored in the 'results' attribute.
 
 =head1 SYNOPSIS 
@@ -30,12 +25,10 @@ see Ecenter::DataClient, Ecenter::Client for the subclassing examples. Normal us
  use Moose;
  extends 'Ecenter::ADS::Detector';
 
- ## or, if no subclasses required then somehting like
-   
-  my $detector = Ecenter::ADS::Detector->new({ data => \@data_array });
-  my $results = $detector->process_data;
-  ### and results are in the results attribute:
-  $results = $detector->results;
+ after 'process_data' => {
+     # actual implementation
+ };
+ 
 
 =head1 ATTRIBUTES
 
@@ -55,17 +48,17 @@ logging  agent via C<Log::Log4perl>
 
 =cut
 
-has data      =>  (is => 'rw', isa => 'HashRef');
-has results   =>  (is => 'rw', isa => 'HashRef');
-has logger    =>  (is => 'rw', isa => 'Log::Log4perl::Logger');
+has data        =>  (is => 'rw', isa => 'HashRef');
+has results     =>  (is => 'rw', isa => 'HashRef');
+has logger      =>  (is => 'rw', isa => 'Log::Log4perl::Logger');
 has parsed_data =>  (is => 'rw', isa => 'HashRef');
-has data_type =>  (is => 'rw', isa => 'Str', default => 'owamp');
+has data_type   =>  (is => 'rw', isa => 'Str', required => 1);
 
 my $METRIC = {owamp => [qw/max_delay min_delay/], bwctl => [qw/throughput/]};
 
 sub BUILD {
     my ($self, $args) = @_;
-    $self->logger(get_logger(__PACKAGE__)); 
+    $self->logger(get_logger(__PACKAGE__));
     map {$self->$_($args->{$_}) if $self->can($_)}  keys %$args if $args && ref $args eq ref {};
 }
 
@@ -75,10 +68,9 @@ sub BUILD {
 
 =cut
 
-
 sub process_data {
-
-
+    my ( $self, $params ) = @_;
+    map {$self->$_($params->{$_}) if $self->can($_)} keys %$params if $params && ref $params eq ref {};
 }
 
 =head1 parse_data
@@ -98,13 +90,13 @@ sub parse_data {
     foreach my $src_ip (keys %$data){
     	foreach my $dst_ip (keys %{$data->{$src_ip}}){
     	    foreach my $timestamp (keys %{$data->{$src_ip}{$dst_ip}}){
-    	    	foreach my $name (@{$METRICS->{$self->data_type}}) {
+    	    	foreach my $name (@{$METRIC->{$self->data_type}}) {
 		    $parsed_data->{$name}{"$src_ip:$dst_ip"}{$timestamp} = $data->{$src_ip}{$dst_ip}{$timestamp}{$name};
     	        }
     	    }
         }
     }
-    foreach my $name (@{$METRICS->{$self->data_type}}) {
+    foreach my $name (@{$METRIC->{$self->data_type}}) {
 	foreach my $key (keys   %{$parsed_data->{$name}}) { 
 	    @{$parsed_data->{$name}{$key}} =  sort {$a->[0] <=> $b->[0] } map {[$_ =>  $parsed_data->{$name}{$key}{$_} ]} 
 	                                        keys %{$parsed_data->{$name}{$key}};
@@ -112,36 +104,31 @@ sub parse_data {
     }
     return $self->parsed_data($parsed_data);
 }
-###################### here
+=head1 add_results
+
+add result for the src/dst pair to the anomaly results hashref
+
+=cut
+
 sub add_result {
-    my ($self, $tri_duration, $ele, $data, $status ) = @_;
-    my %para=(
+    my ( $self, $tri_duration, $key, $ele, $status ) = @_;
+    my ($src, $dst) = split(':', $key);
+   
+    my $response = {
 	 plateau_size => $tri_duration,
          swc          => $self->swc,
          sensitivity  => $self->sensitivity,
          elevation1   => $ele->[0],
          elevation2   => $ele->[1],
-    );
+    };
 
-    for (my $i = 0; $i<= $#$status;$i++){
-    	     if($status->[$i] ==  2){
-    		     $d{$$time[$i]}={$data_typestring=>{value=>$$data[$i],anomaly_type=>"plateau",anomaly_value=>"critical"},timestamp=>$$time[$i]};
-    	     }elsif($$status[$i]==1){
-    		     $d{$$time[$i]}={$data_typestring=>{value=>$$data[$i],anomaly_type=>"plateau",anomaly_value=>"warning"},timestamp=>$$time[$i]};
-    		     $cnt++;
-    	     }   
-     }
-     my $json;
-     my $json_text=to_json(\%$parameter);
-     if($cnt==0){
-    	     $json->{$data_type."_anomaly"}{$src}{$dst}="OK";
-    	     $json_text.=to_json(\%$json);
-    	     return \$json_text;
-     }
-     $json->{$data_type."_anomaly"}{$src}{$dst}=\%d;
-
-
-};
+    if($status && !$status->{critical}){
+        $response->{status} = 'OK';
+    } else {
+        $response->{status} = $status;
+    }
+    return $self->results( { %{$self->results},  $src => { $dst => $response} } );
+}
 
 no Moose;
 __PACKAGE__->meta->make_immutable;

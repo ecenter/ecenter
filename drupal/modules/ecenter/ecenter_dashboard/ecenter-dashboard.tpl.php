80512e4959a63030febd7b56e17778412b7ab5ea
David Eads <davideads@gmail.com>
2011-07-13 11:04:22 -0500

    Added custom E-Center login block.

--- drupal/modules/ecenter/ecenter_dashboard/ecenter-dashboard.tpl.php
+++ drupal/modules/ecenter/ecenter_dashboard/ecenter-dashboard.tpl.php
@@ -1,8 +1,12 @@
 <div class="main-content">
   <?php foreach ($main_content as $name => $block): ?>
   <div id="dashboard-<?php print $name; ?>" class="row clearfix">
+    <?php if (!empty($block['subject'])): ?>
     <h2><?php print $block['subject']; ?></h2>
+    <?php endif; ?>
+    <?php if (!empty($block['description'])): ?>
     <div class="description"><?php print $block['description']; ?></div>
+    <?php endif; ?>
     <div class="content"><?php print $block['content']; ?></div>
   </div>
   <?php endforeach; ?>
@@ -11,8 +15,12 @@
 <div class="secondary-content">
   <?php foreach ($secondary_content as $name => $block): ?>
   <div id="dashboard-<?php print $name; ?>" class="row clearfix">
+    <?php if (!empty($block['subject'])): ?>
     <h2><?php print $block['subject']; ?></h2>
+    <?php endif; ?>
+    <?php if (!empty($block['description'])): ?>
     <div class="description"><?php print $block['description']; ?></div>
+    <?php endif; ?>
     <div class="content"><?php print $block['content']; ?></div>
   </div>
   <?php endforeach; ?>

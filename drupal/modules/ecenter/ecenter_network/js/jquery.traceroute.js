b5f9305c1cff71d5c76234a8786730979b6cf9b1
David Eads <davideads@gmail.com>
2011-09-30 13:12:03 -0500

    Fix arrow placement, add configurable interactivity.

--- drupal/modules/ecenter/ecenter_network/js/jquery.traceroute.js
+++ drupal/modules/ecenter/ecenter_network/js/jquery.traceroute.js
@@ -34,10 +34,7 @@
  *
  * Because of the complicated and data-dependent nature of the drawing routine,
  * no effort has been made to generalize the plugin to support configurable
- * rendering callbacks or similar customization tricks. However, the rendering
- * pipeline is split into several helper functions ($.traceroute.drawMarker(),
- * $.traceroute.drawLabel() and $.traceroute.drawConnectors()) that can be
- * overridden.
+ * rendering callbacks or similar customization tricks. 
  *
  * See $.fn.traceroute.defaults for an overview of traceroute options.
  */
@@ -127,7 +124,8 @@ $.traceroute = function(el, data, options) {
     'size' : (2 * parseInt(options.marker.radius)) + parseInt(options.marker.style['stroke-width']),
     'xOffset' : options.link.width / 2,
     'forwardYOffset' : parseInt(options.marker.radius) + (parseInt(options.marker.style['stroke-width']) / 2),
-    'reverseYOffset' : parseInt(options.marker.radius) + (parseInt(options.marker.style['stroke-width']) / 2) + (parseInt(options.label.style['font-size']) / 2) 
+    'reverseYOffset' : parseInt(options.marker.radius) + (parseInt(options.marker.style['stroke-width']) / 2)
+      + (parseInt(options.label.style['font-size']) / 2) 
   };
 
   this.label = {
@@ -233,7 +231,7 @@ $.traceroute.prototype.draw = function() {
       for (var j = 0; j < step[direction].length; j++) {
         var hop = step[direction][j],
           labelOffset = yOffset + this.label.yOffset,
-          set = paper.set(hop.hub);
+          set = paper.set(hop.hub_name);
 
         if (direction == 'forward' && step['reverse'].length) {
           var markerYOffset = yOffset + this.marker.forwardYOffset;
@@ -259,7 +257,6 @@ $.traceroute.prototype.draw = function() {
         }
  
         // Draw label
-        set = paper.set(hop.hub);
         var label = paper.text(boxOffset + this.marker.xOffset, labelOffset, hop.hub)
           .attr(this.options.label.style); 
         
@@ -267,7 +264,9 @@ $.traceroute.prototype.draw = function() {
         var labelWidth = (bbox.width > markerWidth) ? bbox.width : markerWidth;
         var labelCenterOffset = (bbox.width > markerWidth) ? 0 : (markerWidth - bbox.width) / 2;
 
-        labelBox = paper.rect(bbox.x - this.options.labelBox.paddingX - labelCenterOffset, bbox.y - this.options.labelBox.paddingY, labelWidth + (this.options.labelBox.paddingX * 2), bbox.height + (this.options.labelBox.paddingY * 2))
+        labelBox = paper.rect(bbox.x - this.options.labelBox.paddingX - labelCenterOffset, 
+            bbox.y - this.options.labelBox.paddingY, labelWidth + (this.options.labelBox.paddingX * 2),
+            bbox.height + (this.options.labelBox.paddingY * 2))
           .attr(this.options.labelBox.style);
 
         set.push(label, labelBox);
@@ -275,10 +274,12 @@ $.traceroute.prototype.draw = function() {
         // Draw marker
         if (row_type == 'match') {
           var flip = (direction != 'forward') ? true : false;
-          var marker = paper.halfCircle(boxOffset + this.marker.xOffset, markerYOffset, this.options.marker.radius, flip)
+          var marker = paper.halfCircle(boxOffset + this.marker.xOffset, markerYOffset,
+            this.options.marker.radius, flip)
         } else {
           // Note the fudge factor
-          var marker = paper.circle(boxOffset + this.marker.xOffset, markerYOffset, this.options.marker.radius * 1.15)
+          var marker = paper.circle(boxOffset + this.marker.xOffset, markerYOffset, 
+            this.options.marker.radius * 1.15)
         }
         marker.attr(this.options.marker.style);
         set.push(marker);
@@ -286,9 +287,10 @@ $.traceroute.prototype.draw = function() {
         labelBox.toFront();
         label.toFront();
 
-        set.hover($.traceroute.hoverOver, $.traceroute.hoverOut);
+        //set.hover($.traceroute.hoverOver, $.traceroute.hoverOut);
 
-        $.extend(hop, {'type' : row_type, 'direction' : direction, 'ttl' : i, 'xOffset' : boxOffset + this.marker.xOffset, 'yOffset' : markerYOffset });
+        $.extend(hop, {'type' : row_type, 'direction' : direction, 'ttl' : i, 
+          'xOffset' : boxOffset + this.marker.xOffset, 'yOffset' : markerYOffset });
 
         var directionOffset = (direction == 'forward') ? -this.options.link.offset : this.options.link.offset;
       
@@ -299,11 +301,15 @@ $.traceroute.prototype.draw = function() {
          
           // Skip link
           if (direction == 'reverse' && hop.ttl > (l.ttl + 1)) {
-            var path = ["M", l.xOffset, l.yOffset + directionOffset, "C",  l.xOffset, this.height, hop.xOffset, this.height, hop.xOffset, hop.yOffset + directionOffset];
+            var path = ["M", l.xOffset, l.yOffset + directionOffset, "C",  l.xOffset,
+              this.height, hop.xOffset, this.height, hop.xOffset, hop.yOffset + directionOffset];
             var link = paper.path(path.join(' ')).attr(this.options.link.style);
 
+            // Use bbox to align arrow instead of more complex mathematical
+            // tricks to find intersection with bezier curve.
             var link_bbox = link.getBBox();
-            var arrow = paper.triangle(link_bbox.x + (link_bbox.width / 2), link_bbox.y + link_bbox.height, this.options.arrow.size)
+            var arrow = paper.triangle(link_bbox.x + (link_bbox.width / 2), 
+                link_bbox.y + link_bbox.height, this.options.arrow.size)
               .attr(this.options.arrow.style)
               .rotate(180, true);
             arrow.toBack();
@@ -319,11 +325,18 @@ $.traceroute.prototype.draw = function() {
             var rotation = 0;
             
             if (l.yOffset != hop.yOffset) {
-              arrowYOffset = (parseInt(this.options.arrow.size) / 2) + l.yOffset - ((l.yOffset - hop.yOffset) / 2);
-              rotation = (180 / Math.PI) * Math.atan( (l.yOffset - hop.yOffset) / (l.xOffset - hop.xOffset));
+              if (direction == 'reverse') {
+                var arrowYOffset = l.yOffset - ((l.yOffset - hop.yOffset) / 2) + (parseInt(this.options.arrow.size) / 2);
+              }
+              else {
+                var arrowYOffset = l.yOffset - ((l.yOffset - hop.yOffset) / 2) - (parseInt(this.options.arrow.size) / 2);
+              }
+              var rotation = (180 / Math.PI) * Math.atan( (l.yOffset - hop.yOffset) / (l.xOffset - hop.xOffset));
             }
 
-            var arrow = paper.triangle(arrowXOffset, arrowYOffset, this.options.arrow.size).attr(this.options.arrow.style);
+            var arrow = paper.triangle(arrowXOffset, arrowYOffset, this.options.arrow.size)
+              .attr(this.options.arrow.style);
+
             if (direction == 'reverse') {
               arrow.rotate(180 + rotation, true);
             }
@@ -332,6 +345,12 @@ $.traceroute.prototype.draw = function() {
           link.toBack();
         }
         lastHops[direction] = hop;
+
+        for (behavior in this.options.behavior) {
+          var callback = this.options.behavior[behavior];
+          set[behavior].call(set, callback);
+        }
+
       }
     }
   }
@@ -409,9 +428,10 @@ $.fn.traceroute.defaults = {
   
   // Bind callbacks to events fired on each set of hops
   'behavior' : {
-    'click' : $.noop(),
-    'over' : $.noop(),
-    'out'  : $.noop(),
+    // Uncomment to debug
+    //'mouseover'  : function() { console.log('over'); },
+    //'mouseout'  : function() { console.log('out'); },
+    //'click'  : function() { console.log('click'); },
   }
 };
 

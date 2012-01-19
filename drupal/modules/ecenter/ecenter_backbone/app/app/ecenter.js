2da87a4e2627d525fd53a9f5089ca7481ad6607f
David Eads <davideads@gmail.com>
2012-01-19 07:57:08 -0600

    Add d3 library.

--- drupal/modules/ecenter/ecenter_backbone/app/app/ecenter.js
+++ drupal/modules/ecenter/ecenter_backbone/app/app/ecenter.js
@@ -3,7 +3,7 @@ Models: {}, Collections: {}, Router: {}, Views: {}
 };
 
 DataRetrievalService.Models.StatusMessage = Backbone.Model.extend({
-  url: '/drs/status.json'
+  url: '/drs-status/status.json'
 });
 
 DataRetrievalService.Models.Source = Backbone.Model.extend({
@@ -44,17 +44,65 @@ DataRetrievalService.Views.StatusView = Backbone.View.extend({
     this.model.bind('change', this.render, this);
     _.bindAll(this, 'update');
     this.update();
+   
+    this.chart = d3.select("#main").append("svg")
+      .attr("class", "chart")
 
+    this.chart.w = 14,
+    this.chart.h = 100;
+    
+    this.chart
+      .attr("width", this.chart.w * 80)
+      .attr("height", this.chart.h);
+    
+    this.chart.x = d3.scale.linear()
+        .domain([0, 1])
+        .range([0, this.chart.w]);
+   
+    this.chart.y = d3.scale.linear()
+        .domain([0, 28])
+        .rangeRound([0, this.chart.h]); 
+  
+    this.chart.data = [];
   },
   render: function() {
-    var attr = this.model.toJSON();
-    this.el.html('<pre>'+ JSON.stringify(attr, undefined, 4) +'</pre>');
+    var gearman = this.model.get('gearman') || null;
     return this;
   },
 
   update: function() {
-    this.model.fetch({ success: _.bind(function() {
-      setTimeout(this.update, 10000);
+    this.model.fetch({ success: _.bind(function(obj, data) {
+      setTimeout(this.update, 3000);
+      var now = new Date();
+      var time = now.getTime() / 1000;
+      console.log(data['gearman']['drs']['xenmon.fnal.gov']['10121']['dispatch_data']['running']);
+      this.chart.data.push({'time' : time, 'value' : data['gearman']['drs']['xenmon.fnal.gov']['10121']['dispatch_data']['running']});
+      /*[
+        {'time' : time, 'value' : data['gearman']['drs']['xenmon.fnal.gov']['10121']['dispatch_data']['available']},
+        {'time' : time, 'value' : data['gearman']['drs']['xenmon.fnal.gov']['10121']['dispatch_data']['running']},
+        {'time' : time, 'value' : data['gearman']['drs']['xenmon.fnal.gov']['10121']['dispatch_data']['queued']},
+      ]);*/
+      var rect = this.chart.selectAll("rect")
+        .data(this.chart.data, function(d) { return d.time; });
+   
+      rect.enter().insert("rect", "line")
+        .attr("x", _.bind(function(d, i) { return this.x(i + 1) - .5; }, this.chart))
+        .attr("y", _.bind(function(d) { return this.h - this.y(d.value) - .5; }, this.chart))
+        .attr("width", this.chart.w)
+        .attr("height", _.bind(function(d) { return this.y(d.value); }, this.chart))
+      .transition()
+        .duration(500)
+        .attr("x", _.bind(function(d, i) { return this.x(i) - .25; }, this.chart));
+      
+      rect.transition()
+       .duration(500)
+       .attr("x", _.bind(function(d, i) { return this.x(i) - .25; }, this.chart));
+
+      rect.exit().transition()
+        .duration(500)
+        .attr("x", _.bind(function(d, i) { return this.x(i - 1) - .25; }, this.chart))
+        .remove();
+
     }, this)});  
   }
 });
@@ -80,7 +128,9 @@ DataRetrievalService.Router = Backbone.Router.extend({
   
   sources: function() {
     var sources = new DataRetrievalService.Views.SourceList;
-    sources.render();
+    sources.collection.fetch({success: function() {
+      sources.render();
+    }});
   },
 
   query: function() {

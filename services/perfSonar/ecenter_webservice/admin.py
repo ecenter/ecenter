259bcbac3470f3df956612609cb089c2be22e75b
Maxim Grigoriev <maxim@fnal.gov>
2012-02-10 14:15:34 -0600

    cleanup

--- services/perfSonar/ecenter_webservice/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from ecenter_webservice.models import Metadata, Node, Service
-
-admin.site.register(Metadata)
-admin.site.register(Node)
-admin.site.register(Service)
-
